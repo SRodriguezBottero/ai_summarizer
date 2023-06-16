530de3f57b14fac5a4465f2bc05d993835036e77
Sebastian Endava <sebastiandario.rodriguez@endava.com>
2023-06-16 08:56:12 -0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+
+        <button
+          type='button'
+          onClick={() => window.open('https://github.com/SRodriguezBottero')}
+          className='black_btn'
+        >
+          GiHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'></h1>
+    </header>
+  )
+}
+
+export default Hero
