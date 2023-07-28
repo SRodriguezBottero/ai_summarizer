2eb18f3a096d43d33448fa97f901c9ebf629ff22
Sebastian Endava <sebastiandario.rodriguez@endava.com>
2023-07-28 08:56:07 -0300

    Removi el boton al condigo de Github

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,7 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
 
-        <button
-          type='button'
-          onClick={() => window.open('https://github.com/SRodriguezBottero')}
-          className='black_btn'
-        >
-          GiHub
-        </button>
+
       </nav>
 
       <h1 className='head_text'>
